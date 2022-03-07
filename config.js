d102abe4419b6ab233f22238c15fc92a33b94237
CoderIT2001 <96308013+CoderIT2001@users.noreply.github.com>
2022-03-07 15:57:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Công Chúa';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng Sinh Nhật';
+const eventName = 'Chúc Mừng Ngày 8-3';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc em có 1 ngày sinh nhật tràn ngập niềm vui và những nụ cười. Chúc em luôn luôn vui vẻ và đừng buồn nữa nhé. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc em có 1 ngày 8-3 tràn ngập niềm vui và những nụ cười. Chúc em luôn luôn vui vẻ và đừng buồn nữa nhé. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
